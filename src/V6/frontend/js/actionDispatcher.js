58575c6087032303feade0a88856dc7766461e7c
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 14:50:06 +0530

    folder fix

--- src/V6/frontend/js/actionDispatcher.js
+++ src/V6/frontend/js/actionDispatcher.js
@@ -3,23 +3,31 @@ function getSelectedTable() {
     return tableSelect ? tableSelect.value : "";
 }
 
-function getFolderName() {
+function getFolderName(defaultValue = "") {
     const folderInput = document.getElementById("folder-name");
-    return folderInput ? folderInput.value.trim() : "";
+    if (folderInput) {
+        let val = folderInput.value.trim();
+        if (!val && defaultValue) {
+            folderInput.value = defaultValue;
+            val = defaultValue;
+        }
+        return val;
+    }
+    return "";
 }
 
 function insertGenPk() {
-    sendAction("insertGenPk", { tableName: getSelectedTable(), inFolderName: getFolderName() });
+    sendAction("insertGenPk", { tableName: getSelectedTable(), inFolderName: getFolderName("insertGenPk") });
 }
 
 function find() {
-    sendAction("filter", { tableName: getSelectedTable(), inFolderName: getFolderName() });
+    sendAction("filter", { tableName: getSelectedTable(), inFolderName: getFolderName("find") });
 }
 
 function filterQuery() {
-    sendAction("filterQuery", { tableName: getSelectedTable(), inFolderName: getFolderName() });
+    sendAction("filterQuery", { tableName: getSelectedTable(), inFolderName: getFolderName("filterQuery") });
 };
 
 const groupBy = () => {
-    sendAction("groupBy", { tableName: getSelectedTable(), inFolderName: getFolderName() });
+    sendAction("groupBy", { tableName: getSelectedTable(), inFolderName: getFolderName("groupBy") });
 };
