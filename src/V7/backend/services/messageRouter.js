cd40b3a6b1ec489c00c11112cfdd220a9522e1af
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 21:35:30 +0530

    7

---
+++ src/V7/backend/services/messageRouter.js
@@ -0,0 +1,62 @@
+import findAction from "./actions/find.js";
+import insertGenPkAction from "./actions/insertGenPk.js";
+import filterQueryAction from "./actions/filterQuery.js";
+import groupByAction from "./actions/groupBy.js";
+import { getSchemaFiles } from "./schemaService.js";
+
+export async function handleWebviewMessage({ message, panel, toPath, schemasPath,
+    port, inTargetPath }) {
+    switch (message.action) {
+        case "loadSchemas":
+            const schemas = getSchemaFiles(schemasPath);
+            panel.webview.postMessage({
+                type: "schemas",
+                schemas
+            });
+            break;
+
+        case "insertGenPk":
+            await insertGenPkAction({
+                panel,
+                tableName: message.tableName,
+                toPath,
+                schemasPath,
+                inFolderName: message.inFolderName,
+                inTargetPath,
+                inPort: port
+            });
+            break;
+
+        case "filter":
+            await findAction({
+                panel,
+                tableName: message.tableName,
+                toPath,
+                schemasPath,
+                inFolderName: message.inFolderName
+            });
+            break;
+
+        case "filterQuery":
+            await filterQueryAction({
+                panel,
+                tableName: message.tableName,
+                toPath,
+                schemasPath,
+                inFolderName: message.inFolderName
+            });
+            break;
+
+        case "groupBy":
+            await groupByAction({
+                panel,
+                tableName: message.tableName,
+                toPath,
+                schemasPath,
+                inFolderName: message.inFolderName,
+                inTargetPath,
+                inPort: port
+            });
+            break;
+    }
+};
